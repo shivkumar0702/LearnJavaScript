7e90271b9433dee75dd793ea77e7daee1d69f26d
shivkumar0702 <shivamkumawat2000@gmail.com>
2025-04-02 11:51:16 +0530

    1st April 2025

---
+++ DataType, Data type conversion.js
@@ -0,0 +1,17 @@
+         // Data Types......             
+const accountId = 12345
+let accountEmail = "hc@hc.com"
+var accountPassword = "21212121"
+accountCity = "Jaipur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]); 
+console.log(accountCity);
+
+      // Conversion
+let score =  30
+console.log(typeof(score));
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); 
