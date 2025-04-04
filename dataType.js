5a566da32c11c1185177dfd6d5b4321edd0a1078
shivkumar0702 <shivamkumawat2000@gmail.com>
2025-04-04 13:05:14 +0530

    Learning datatypes and create stopwatch

---
+++ dataType.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+ let accountEmail = "hc@hc.com"
+ var accountPassword = "21212121"
+ accountCity = "Jaipur"
+ 
+ console.table([accountEmail,accountId,accountPassword,accountCity]); 
+ console.log(accountCity);
+ 
+ let score =  30
+ console.log(typeof(score));
+ 
+ let someNumber =33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber); 
