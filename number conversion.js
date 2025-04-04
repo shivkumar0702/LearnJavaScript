a8182fa67b8d5b99b024594fab85a77ac1681b2d
shivkumar0702 <shivamkumawat2000@gmail.com>
2025-04-04 14:39:11 +0530

    Perform Number conversion

---
+++ number conversion.js
@@ -0,0 +1,27 @@
+//************* */ Number Conversion
+
+let value = 30     
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Shiv"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+ 
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 
+
+
+let gameCounter = 100     //**Increment */
+gameCounter++;
+console.log(gameCounter);
